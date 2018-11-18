059929f8c3e738633cd987113b247b8cc0ae8b19
skustra <kustraslawomir@gmail.com>
2018-11-18 11:52:17 +0100

    mvvm architecture

--- app/src/main/java/slawomir/kustra/kodeinexample/di/modules/AppModules.kt
+++ app/src/main/java/slawomir/kustra/kodeinexample/di/modules/AppModules.kt
@@ -3,7 +3,16 @@ package slawomir.kustra.kodeinexample.di.modules
 import android.content.Context
 import org.kodein.di.Kodein
 import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.generic.provider
 import org.kodein.di.generic.singleton
+import slawomir.kustra.kodeinexample.data.database.Database
+import slawomir.kustra.kodeinexample.data.database.DatabaseImpl
+import slawomir.kustra.kodeinexample.data.database.QuoteDao
+import slawomir.kustra.kodeinexample.data.database.QuoteDaoImpl
+import slawomir.kustra.kodeinexample.data.repository.QuoteRepository
+import slawomir.kustra.kodeinexample.data.repository.QuoteRepositoryImpl
+import slawomir.kustra.kodeinexample.ui.activity.QuotesViewModelFactory
 import slawomir.kustra.kodeinexample.utils.logger.Logger
 import slawomir.kustra.kodeinexample.utils.logger.LoggerIml
 
@@ -12,5 +21,16 @@ Binding
  */
 fun appModule(context: Context) = Kodein.Module {
 
-   bind<Logger>() with singleton { LoggerIml() }
+    bind<Logger>() with singleton { LoggerIml() }
+
+    bind<Database>() with singleton { DatabaseImpl() }
+
+    bind<QuoteDao>() with singleton { instance<Database>().quoteDao }
+
+    bind<QuoteRepository>() with singleton { QuoteRepositoryImpl(instance()) }
+
+    /*
+    provides -> new instance in opposite to singleton
+     */
+    bind() from provider { QuotesViewModelFactory(instance()) }
 }
--- app/src/main/java/slawomir/kustra/kodeinexample/ui/activity/MainActivity.kt
+++ app/src/main/java/slawomir/kustra/kodeinexample/ui/activity/MainActivity.kt
@@ -2,14 +2,39 @@ package slawomir.kustra.kodeinexample.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.closestKodein
+import org.kodein.di.generic.instance
 import slawomir.kustra.kodeinexample.R
+import slawomir.kustra.kodeinexample.data.model.Quote
+import java.lang.StringBuilder
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), KodeinAware {
+
+    override val kodein by closestKodein()
+
+    private val quotesViewModelFactory: QuotesViewModelFactory by instance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+
+        val viewModel = ViewModelProviders.of(this, quotesViewModelFactory).get(QuotesViewModel::class.java)
+
+        fab.setOnClickListener {
+            viewModel.addQuote(Quote("Some fancy quote", "Johny  Bravo"))
+        }
+
+        viewModel.getQuotes().observe(this, Observer { quotes ->
+            val stringBuilder = StringBuilder()
+            quotes.forEach { quote ->
+                stringBuilder.append(quote.toString())
+            }
+            quotesTextView.text = stringBuilder.toString()
+        })
     }
 }
